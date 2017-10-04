889aea387e13554e5ebe49b24b28794e19efcc8d
Evgenii Fedoseev <abap@sapville.com>
2017-10-04 12:49:29 +0300

    feat: draw the grid
    Create the grid table with the dimensions chosen by a users in input
    fields

--- js/designs.js
+++ js/designs.js
@@ -9,6 +9,29 @@ function main() {
 
 function makeGrid() {
 
-  // Your code goes here!
+  const height = $.url('?height');
+  const width = $.url('?width');
 
+  function concatColString() {
+    let colString = '';
+    for (let i = 0; i < width; i++) {
+      colString += '<td></td>';
+    }
+    return colString;
+  }
+
+  if (height === undefined || width === undefined || height === 0 || width === 0) {
+    $('#input_height').val(1);
+    $('#input_width').val(1);
+    return;
+  }
+
+  $('#input_height').val(height);
+  $('#input_width').val(width);
+
+  const pageTable = $('#pixel_canvas');
+  for (let i = 0; i < height; i++) {
+    pageTable.append($('<tr>'));
+    pageTable.children('tr').last().append($(concatColString()));
+  }
 }
